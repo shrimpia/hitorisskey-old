fba0d3bf5724d4c705cf18d95634848af7842963
Xeltica <xeltica@gmail.com>
2021-04-21 08:36:24 +0900

    Remove Vue and Old Client

--- src/client/widgets/index.ts
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue';
-
-Vue.component('mkw-memo', () => import('./memo.vue').then(m => m.default));
-Vue.component('mkw-calendar', () => import('./calendar.vue').then(m => m.default));
-Vue.component('mkw-rss', () => import('./rss.vue').then(m => m.default));
-Vue.component('mkw-clock', () => import('./clock.vue').then(m => m.default));
-Vue.component('mkw-activity', () => import('./activity.vue').then(m => m.default));
-Vue.component('mkw-photos', () => import('./photos.vue').then(m => m.default));
